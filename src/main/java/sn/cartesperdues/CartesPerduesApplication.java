754ddcb4b12bfa4da2a4d6e6ee43288c058a308f
AbdouGUEYE4 <a.g59@zig.univ.sn>
2025-12-24 16:59:43 +0000

    Initialisation du projet Spring Boot

---
+++ src/main/java/sn/cartesperdues/CartesPerduesApplication.java
@@ -0,0 +1,13 @@
+package sn.cartesperdues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartesPerduesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartesPerduesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/cartesperdues/CartesPerduesApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.cartesperdues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartesPerduesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
